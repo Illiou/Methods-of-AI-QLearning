677526bbed6a4a95b54eebef8418e237b9c967bc
Emanuel Roos <manny123@hotmail.de>
2018-03-05 06:45:20 +0100

    Update DefaultConstants.py

--- DefaultConstants.py
+++ DefaultConstants.py
@@ -11,18 +11,19 @@ ACTIONS = [(0, -1), (1, 0), (0, 1), (-1, 0)]
 TRANSITION_PROBABILITIES = {"straight": 0.8, "lateral": 0.1}
 
 # default values for Q-learning
-DISCOUNT_FACTOR = 1
+DISCOUNT_FACTOR = 1.0
 LEARNING_RATE = 0.1
-EPSILON = 0.8
+EPSILON = 0.5
 CONVERGENCE_THRESHOLD = 100
 
 # default values for pretty printing
 FIELD_MAPPING = {"F": " ", "O": "■", "E": "+", "P": "-"}
 BOUNDARY_CHAR = "█"
 ACTION_MAPPING = {(0, -1): "↑", (1, 0): "→", (0, 1): "↓", (-1, 0): "←", (0, 0): " "}
-OUTPUT_WIDTH = 80
+OUTPUT_WIDTH = 90
 
 # other
+SLEEP_TIME = 1.5
 YES_SYNONYMS = ["y", "yes", "ye", "yea", "yeah", "yep", "yay", "aye", "arr", "sure",
                 "j", "ja", "jap", "jep", "jo", "joa"]
 NO_SYNONYMS = ["n", "no", "nay", "nope", "exit", "quit", "stop", "nein", "ne", "nö"]
